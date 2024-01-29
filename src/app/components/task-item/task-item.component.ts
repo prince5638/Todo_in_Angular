5798a0e996eb899f8de4d549e946413cca52feff
Prince Changani <princechangani56385638@gmail.com>
2024-01-29 15:32:12 +0530

    Deleting the task using services
    Signed-off-by: Prince Changani <princechangani56385638@gmail.com>

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -13,8 +13,15 @@ import { faTimes } from '@fortawesome/free-solid-svg-icons';
 })
 export class TaskItemComponent {
   @Input() task: Task = {text: "",day: "", reminder: false};
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
  
   faTimes = faTimes;
+
+  // Deleting the task
+  onDelete(task: any)
+  {
+    this.onDeleteTask.emit(task);
+  }
 
   
 }
